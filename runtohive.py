e5aca4363a71f82e9189eb12fe6d648f85bf789d
Claire <164862759+claire-manassanan@users.noreply.github.com>
2025-07-22 20:30:59 +0700

    Update runtohive.py
    update from modeling into just cleansing data

--- runtohive.py
+++ runtohive.py
@@ -13,97 +13,67 @@ def main():
             .config('spark.sql.catalogImplementation', 'hive')\
             .config('hive.metastore.client.factory.class',
                 'com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory')\
-
             .getOrCreate()
-
+    
+    sss_schema = StructType([
+        StructField('row_id', StringType(), True),
+        StructField('order_id', StringType(), True),
+        StructField('order_date', DateType(), True),
+        StructField('ship_date', DateType(), True),
+        StructField('ship_mode', StringType(), True),
+        StructField('customer_id', StringType(), True),
+        StructField('customer_name', StringType(), True),
+        StructField('segment', StringType(), True),
+        StructField('country', StringType(), True),
+        StructField('city', StringType(), True),
+        StructField('state', StringType(), True),
+        StructField('postal_code', LongType(), True),
+        StructField('region', StringType(), True),
+        StructField('product_id', StringType(), True),
+        StructField('category', StringType(), True),
+        StructField('sub_category', StringType(), True),
+        StructField('product_name', StringType(), True),
+        StructField('sales', DoubleType(), True),
+        StructField('quantity', LongType(), True),
+        StructField('discount', DoubleType(), True),
+        StructField('profit', DoubleType(), True)
+    ])
+    
     df = spark.read.format('csv')\
-                    .option('header',True)\
-                    .option('inferSchema',True)\
+                    .schema(sss_schema)\
+                    .option('header',False)\
                     .load(S3_INPUT_PATH)
 
-    fact = df.select(col('Order ID')\
-                    ,col('Order Date').cast(DateType()), col('Ship Date').cast(DateType())\
-                    ,col('Ship Mode'), col('Customer ID')\
-                    ,col('Postal Code'), col('Product ID')\
-                    ,col('Sales').cast('float'), col('Quantity').cast('int'), col('Discount').cast('float')
-                    )
-  
-    dim_cus = df.select(col('Customer ID'), col('Customer Name')\
-                    ,col('Segment')
-                    )\
-                    .dropDuplicates()
+    # then clean some data
+    df = df.drop('row_id')
 
-    dim_country = df.select(col('Postal Code'), col('Country')\
-                    ,col('City'), col('State'), col('Region')
-                    )\
-                    .dropDuplicates()
-
-    dim_product = df.select(col('Product ID'), col('Category')\
-                    ,col('Sub-Category'), col('Product Name')
-                    )\
-                    .dropDuplicates()
-
-    spark.sql("CREATE DATABASE IF NOT EXISTS sss;")
-    spark.sql(f"CREATE TABLE IF NOT EXISTS fact_order(\
+    spark.sql("CREATE DATABASE IF NOT EXISTS sss_silver;")
+    spark.sql(f"CREATE EXTERNAL TABLE IF NOT EXISTS sss_silver.transaction(\
                 order_id VARCHAR(100),\
-                order_date DATE,\
                 ship_date DATE,\
                 ship_mode VARCHAR(100),\
                 customer_id VARCHAR(100),\
-                postal_code INT,\
-                product_id VARCHAR(100),\
-                sales DECIMAL(10,2),\
-                quantity INT,\
-                discount DECIMAL(10,2)\
-                )\
-                ROW FORMAT DELIMITED FIELDS TERMINATED BY ','\
-                STORED AS TEXTFILE;\
-                LOCATION '{S3_OUTPUT_PATH}'
-                ")
-            
-    fact.createOrReplaceTempView('fact_tmp')
-    spark.sql('INSERT INTO sss.fact_order SELECT * FROM fact_tmp;')
-
-    spark.sql(f"CREATE EXTERNAL TABLE IF NOT EXISTS dim_cus(\
-                customer_id VARCHAR(100),\
                 customer_name VARCHAR(250),\
-                segment VARCHAR(100)\
-                )\
-                ROW FORMAT DELIMITED FIELDS TERMINATED BY ','\
-                STORED AS TEXTFILE;\
-                LOCATION '{S3_OUTPUT_PATH}'\
-                ")
-            
-    dim_cus.createOrReplaceTempView('dim_cus_tmp')
-    spark.sql('INSERT INTO sss.dim_cus SELECT * FROM dim_cus_tmp;')
-
-    spark.sql(f"CREATE EXTERNAL TABLE IF NOT EXISTS dim_country(\
-                postal_code INT,\
-                country VARCHAR(250),\
+                segment VARCHAR(150),\
+                country VARCHAR(150),\
                 city VARCHAR(100),\
                 state VARCHAR(100),\
-                region VARCHAR(100)\
-                )\
-                ROW FORMAT DELIMITED FIELDS TERMINATED BY ','\
-                STORED AS TEXTFILE;\
-                LOCATION '{S3_OUTPUT_PATH}'\
-                ")
-            
-    dim_country.createOrReplaceTempView('dim_country_tmp')
-    spark.sql('INSERT INTO sss.dim_country SELECT * FROM dim_country_tmp;')
-
-    spark.sql(f"CREATE EXTERNAL TABLE IF NOT EXISTS dim_product(\
+                region VARCHAR(100),\
+                postal_code INT,\
                 product_id VARCHAR(100),\
                 catagory VARCHAR(100),\
                 sub_catagory VARCHAR(100),\
                 product_name VARCHAR(250)\
+                sales DECIMAL(10,2),\
+                quantity INT,\
+                discount DECIMAL(10,2),\
+                profit DECIMAL(10,2)
                 )\
-                ROW FORMAT DELIMITED FIELDS TERMINATED BY ','\
-                STORED AS TEXTFILE;\
+                PARTITION BY (order_date DATE)\
+                STORED AS PARGUET\
                 LOCATION '{S3_OUTPUT_PATH}'\
                 ")
-            
-    dim_product.createOrReplaceTempView('dim_product_tmp')
-    spark.sql('INSERT INTO sss.dim_product SELECT * FROM dim_product_tmp;')
+
+    df.write.mode('overwrite').partitionBy(col('order_date')).insertInto('sss_silver.transaction')
     
 main()
